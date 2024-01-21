5e1370e67df3fb2c11e6cf881050a9b0f012beb8
Grzegorz02@ <grzegorz.perun@gmail.com>
2024-01-21 19:22:12 +0100

    feat: home page adjustments

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="flex h-screen flex-col justify-center px-32">
+  <div class="px-12 py-9">
     <div class="mb-8">
-      <h1 class="mb-4 text-5xl font-bold text-gray-900">
+      <h1 class="mb-4 text-4xl font-bold text-gray-900">
         Plan SGGW
       </h1>
       <p class="mb-6 text-gray-700">
@@ -9,32 +9,38 @@
       </p>
 
       <div class="flex flex-col gap-2">
-        <div class="flex gap-6">
-          <div class="w-full cursor-pointer rounded-lg border border-gray-200 p-6 transition-all duration-200 hover:shadow-lg">
-            <NuxtLink to="/schedules" class="mb-2 block text-2xl font-bold text-gray-700 transition-colors duration-200 hover:text-gray-900">
-              Plany zajęć
-            </NuxtLink>
-            <p class="text-gray-600">
-              W tym miejscu możesz sprawdzić plany zajęć dla wybranych kierunków studiów.
-            </p>
+        <div class="grid auto-rows-[1fr] grid-cols-3 gap-6">
+          <div class="group w-full">
+            <div class="cursor-pointer rounded-lg border border-gray-200 p-6 transition-all duration-200 hover:shadow-lg group-hover:bg-indigo-600 h-full">
+              <NuxtLink to="/schedules" class="mb-2 block text-2xl font-bold text-gray-700 transition-colors duration-200 group-hover:text-white">
+                Plany zajęć
+              </NuxtLink>
+              <p class="text-gray-600 group-hover:text-white">
+                W tym miejscu możesz sprawdzić plany zajęć dla wybranych kierunków studiów.
+              </p>
+            </div>
           </div>
 
-          <div class="w-full cursor-pointer rounded-lg border border-gray-200 p-6 transition-all duration-200 hover:shadow-lg">
-            <NuxtLink to="/lecturers" class="mb-2 block text-2xl font-bold text-gray-700 transition-colors duration-200 hover:text-gray-900">
-              Prowadzący
-            </NuxtLink>
-            <p class="text-gray-600">
-              W tym miejscu możesz sprawdzić prowadzących. Lista jest globalna i obejmuje Bazę Wiedzy SGGW.
-            </p>
+          <div class="group w-full">
+            <div class="cursor-pointer rounded-lg border border-gray-200 p-6 transition-all duration-200 hover:shadow-lg group-hover:bg-indigo-600 h-full">
+              <NuxtLink to="/lecturers" class="mb-2 block text-2xl font-bold text-gray-700 transition-colors duration-200 group-hover:text-white">
+                Prowadzący
+              </NuxtLink>
+              <p class="text-gray-600 group-hover:text-white">
+                W tym miejscu możesz sprawdzić prowadzących. Lista jest globalna i obejmuje Bazę Wiedzy SGGW.
+              </p>
+            </div>
           </div>
 
-          <div class="w-full cursor-pointer rounded-lg border border-gray-200 p-6 transition-all duration-200 hover:shadow-lg">
-            <NuxtLink to="/classrooms" class="mb-2 block text-2xl font-bold text-gray-700 transition-colors duration-200 hover:text-gray-900">
-              Sale
-            </NuxtLink>
-            <p class="text-gray-600">
-              W tym miejscu możesz sprawdzić sale.
-            </p>
+          <div class="group w-full">
+            <div class="cursor-pointer rounded-lg border border-gray-200 p-6 transition-all duration-200 hover:shadow-lg group-hover:bg-indigo-600 h-full">
+              <NuxtLink to="/classrooms" class="mb-2 block text-2xl font-bold text-gray-700 transition-colors duration-200 group-hover:text-white">
+                Sale
+              </NuxtLink>
+              <p class="text-gray-600 group-hover:text-white">
+                W tym miejscu możesz sprawdzić sale.
+              </p>
+            </div>
           </div>
         </div>
       </div>
@@ -66,7 +72,7 @@
       </div>
     </div>
 
-    <small class="text-right text-gray-700">
+    <small class="block w-full text-right text-gray-700">
       Projekt realizowany przez <a href="mailto:kontakt@silver.sggw.pl" class="font-semibold transition-colors duration-200 hover:text-gray-900">Koło Naukowe Informatyków SGGW.</a>
     </small>
   </div>
