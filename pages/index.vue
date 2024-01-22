1cbc7a178dad7fc140fa3044530f4f3c119d5208
Grzegorz02@ <grzegorz.perun@gmail.com>
2024-01-22 09:57:43 +0100

    feat(home): include authors name

--- pages/index.vue
+++ pages/index.vue
@@ -73,7 +73,7 @@
     </div>
 
     <small class="block w-full text-right text-gray-700">
-      Projekt realizowany przez <a href="mailto:kontakt@silver.sggw.pl" class="font-semibold transition-colors duration-200 hover:text-gray-900">Koło Naukowe Informatyków SGGW.</a>
+      Projekt realizowany przez <a href="mailto:kontakt@silver.sggw.pl" class="font-semibold transition-colors duration-200 hover:text-gray-900">Koło Naukowe Informatyków SGGW Silver.NET.</a>
     </small>
   </div>
 </template>
