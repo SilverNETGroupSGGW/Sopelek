3a0d6757f14b302495e86c92f69fbccef6dcdef1
Grzegorz02@ <grzegorz.perun@gmail.com>
2024-03-09 17:58:47 +0100

    feat: toggle route id query when updating item

--- composables/useCrud.ts
+++ composables/useCrud.ts
@@ -10,6 +10,18 @@ export default function useCrud<T extends { id: string }>(items: T[]) {
 
   const isSubmitting = ref(false)
 
+  // Cleanup route query when dialog is closed
+  watch(updateDialog, (value, previousValue) => {
+    if (!value && previousValue) {
+      const query = { ...route.query }
+      delete query.id
+      navigateTo({ path: route.path, query })
+    }
+  })
+
+  if (route.query.id)
+    handleDialogOpen('update', route.query.id as string)
+
   async function handleCreate(currentItem: T, callback: () => void) {
     isSubmitting.value = true
     callback()
@@ -34,16 +46,26 @@ export default function useCrud<T extends { id: string }>(items: T[]) {
     isSubmitting.value = false
   }
 
-  function handleDialogOpen(mode: 'create' | 'update' | 'delete', id?: string) {
+  async function handleDialogOpen(mode: 'create' | 'update' | 'delete', id?: string) {
     if (id) {
       const item = items.find(item => item.id === id)
       if (item)
         currentItem.value = { ...item }
 
-      if (mode === 'update')
+      if (mode === 'update') {
+        if (!route.query.id) {
+          const route = useRoute()
+          await navigateTo({
+            path: route.path,
+            query: { ...route.query, id },
+          })
+        }
+
         updateDialog.value = true
-      else
+      }
+      else {
         deleteDialog.value = true
+      }
     }
     else {
       currentItem.value = {} as T
