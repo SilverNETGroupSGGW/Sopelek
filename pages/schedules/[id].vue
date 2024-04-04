b5910f1221a391570788c0e8d0f9c4bff7031ce8
Grzegorz02@ <grzegorz.perun@gmail.com>
2024-04-04 20:16:37 +0200

    feat: schedules view

--- pages/schedules/[id].vue
+++ pages/schedules/[id].vue
@@ -1,39 +1,37 @@
 <script setup lang="ts">
-import { CalendarIcon, ClockIcon, MagnifyingGlassIcon, TrashIcon, TrophyIcon, UserIcon, ViewfinderCircleIcon } from '@heroicons/vue/24/solid'
+import { CalendarIcon, ChartBarIcon, MagnifyingGlassIcon, TrashIcon, UserIcon, ViewfinderCircleIcon } from '@heroicons/vue/24/solid'
 import { DialogClose, DialogDescription } from 'radix-vue'
 
+const route = useRoute()
+
 // Supabase
-const studyPlans = useStudyPlans()
-await studyPlans.get()
+const schedules = useSchedules()
+await schedules.get(Number.parseInt(route.params.id as string))
 
 // Utils
-const data = useData()
-
-const { mapArrayToLabelValue } = useArray()
-const { currentItem, createDialog, deleteDialog, handleCreate, handleDelete, handleDialogOpen, handleUpdate, isSubmitting, search, updateDialog } = useCrud(studyPlans.data)
+const { currentItem, createDialog, deleteDialog, handleCreate, handleDelete, handleDialogOpen, handleUpdate, isSubmitting, search, updateDialog } = useCrud(schedules.data)
 </script>
 
 <template>
   <div class="flex w-full flex-wrap items-center justify-between gap-4 border-b bg-gray-50 px-12 py-4">
     <h1 class="text-2xl font-bold leading-9 text-gray-950">
-      Plany studiów
+      Plany zajęć
     </h1>
 
     <div class="flex items-center gap-4">
       <base-input v-model="search" placeholder="Szukaj" class="w-96" :icon="MagnifyingGlassIcon" />
 
-      <base-dialog :open="createDialog" title="Utwórz plan studiów" :icon="UserIcon">
+      <base-dialog :open="createDialog" title="Utwórz plan zajęć" :icon="UserIcon">
         <template #trigger>
           <base-button class="h-12" variant="primary" @click="handleDialogOpen('create')">
-            Dodaj plan studiów
+            Dodaj plan zajęć
           </base-button>
         </template>
 
-        <form class="flex flex-col gap-4" @submit.prevent="handleCreate(currentItem, async() => await studyPlans.create(currentItem))">
+        <form class="flex flex-col gap-4" @submit.prevent="handleCreate(currentItem, async() => await schedules.create(currentItem, Number.parseInt(route.query.id as string)))">
           <base-input v-model="currentItem.start" :icon="CalendarIcon" label="Data rozpoczęcia" />
-          <base-input v-model="currentItem.field" :icon="ViewfinderCircleIcon" label="Kierunek" />
-          <base-select v-model.number="currentItem.type" :icon="TrophyIcon" label="Typ studiów" :options="mapArrayToLabelValue(data.studyTypes)" />
-          <base-select v-model.number="currentItem.mode" :icon="ClockIcon" label="Tryb studiów" :options="mapArrayToLabelValue(data.studyModes)" />
+          <base-input v-model="currentItem.name" :icon="ViewfinderCircleIcon" label="Nazwa" />
+          <base-input v-model="currentItem.term" type="number" :icon="ChartBarIcon" label="Semestr" />
 
           <div class="mt-6 flex justify-end gap-4">
             <DialogClose as-child>
@@ -50,32 +48,30 @@ const { currentItem, createDialog, deleteDialog, handleCreate, handleDelete, han
     </div>
   </div>
 
-  <base-table :search="search" :data="studyPlans.data" :columns="studyPlans.columns">
+  <base-table :search="search" :data="schedules.data" :columns="schedules.columns">
     <template #name="{ cell }">
-      <span class="text-base font-medium text-gray-900">{{ cell.field }}</span>
+      <span class="text-base font-medium text-gray-900">{{ cell.name }}</span>
       <br>
       <span class="text-sm text-gray-700">
-        od roku akademickiego {{ new Date(cell.start).getFullYear() }}/{{ new Date(cell.start).getFullYear() + 1 }}
+        od {{ new Date(cell.start).toLocaleDateString('pl-PL') }}
         <br>
-        {{ data.studyTypes[cell.type - 1] }}, {{ data.studyModes[cell.mode - 1] }}
-        <br>
+        semestr {{ cell.term }}
       </span>
     </template>
 
     <template #actions="{ cell }">
       <div class="flex gap-4">
-        <base-dialog :open="updateDialog" title="Edytuj plan studiów" :icon="UserIcon">
+        <base-dialog :open="updateDialog" title="Edytuj plan zajęć" :icon="UserIcon">
           <template #trigger>
             <base-button variant="primary" @click="handleDialogOpen('update', cell.id)">
               Edytuj
             </base-button>
           </template>
 
-          <form class="flex flex-col gap-4" @submit.prevent="handleUpdate(currentItem, async() => await studyPlans.update(currentItem))">
+          <form class="flex flex-col gap-4" @submit.prevent="handleUpdate(currentItem, async() => await schedules.update(currentItem))">
             <base-input v-model="currentItem.start" :icon="CalendarIcon" label="Data rozpoczęcia" />
-            <base-input v-model="currentItem.field" :icon="ViewfinderCircleIcon" label="Kierunek" />
-            <base-select v-model.number="currentItem.type" :icon="TrophyIcon" label="Typ studiów" :options="mapArrayToLabelValue(data.studyTypes)" />
-            <base-select v-model.number="currentItem.mode" :icon="ClockIcon" label="Tryb studiów" :options="mapArrayToLabelValue(data.studyModes)" />
+            <base-input v-model="currentItem.name" :icon="ViewfinderCircleIcon" label="Nazwa" />
+            <base-input v-model="currentItem.term" type="number" :icon="ChartBarIcon" label="Semestr" />
 
             <div class="mt-6 flex justify-end gap-4">
               <DialogClose as-child>
@@ -90,18 +86,14 @@ const { currentItem, createDialog, deleteDialog, handleCreate, handleDelete, han
           </form>
         </base-dialog>
 
-        <base-button variant="secondary" :to="`/schedules/${cell.id}`">
-          Plany zajęć
-        </base-button>
-
-        <base-dialog :open="deleteDialog" title="Usuń plan studiów" :icon="TrashIcon">
+        <base-dialog :open="deleteDialog" title="Usuń plan zajęć" :icon="TrashIcon">
           <template #trigger>
             <base-button variant="danger" @click="handleDialogOpen('delete', cell.id)">
               Usuń
             </base-button>
           </template>
 
-          <form @submit.prevent="handleDelete(currentItem, async() => await studyPlans.delete(currentItem))">
+          <form @submit.prevent="handleDelete(currentItem, async() => await schedules.delete(currentItem))">
             <DialogDescription class="text-base text-gray-700">
               Czy na pewno chcesz usunąć plan?
             </DialogDescription>
---
+++ stores/useSchedules.ts
@@ -0,0 +1,59 @@
+// // import type { Database } from '~/.'
+
+import type { Database } from '~'
+
+// type StudyPlan = Database['public']['Tables']['study_plans']['Row']
+
+type Schedule = Database['public']['Tables']['schedules']['Row']
+
+export const useSchedules = defineStore('schedules', {
+  state: () => ({
+    search: '',
+    data: [] as Schedule[],
+    columns: [
+      {
+        key: 'name',
+        header: 'Nazwa',
+      },
+      {
+        key: 'actions',
+        header: 'Akcje',
+      },
+    ],
+  }),
+  actions: {
+    async get(studyPlanId: number) {
+      const client = useSupabaseClient<Database>()
+
+      const { data } = await useAsyncData('schedules', async () => {
+        const { data } = await client.from('schedules').select('*').eq('study_plan_id', studyPlanId)
+        return data
+      })
+
+      if (data.value)
+        this.data = data.value
+    },
+    async create(schedule: Schedule, studyPlanId: number) {
+      const client = useSupabaseClient<Database>()
+
+      schedule.is_published = false
+      schedule.study_plan_id = studyPlanId
+
+      const { data } = await client.from('schedules').insert(schedule).select().order('id').limit(1).single()
+      this.data.push(data!)
+    },
+    async update(schedule: Schedule) {
+      const client = useSupabaseClient<Database>()
+
+      const { data } = await client.from('schedules').update(schedule).eq('id', schedule.id).select().order('id').limit(1).single()
+      const index = this.data.findIndex(l => l.id === data!.id)
+      this.data[index] = data!
+    },
+    async delete(schedule: Schedule) {
+      const client = useSupabaseClient<Database>()
+
+      await client.from('schedules').delete().eq('id', schedule.id)
+      this.data = this.data.filter(l => l.id !== schedule.id)
+    },
+  },
+})
