5e50945db0a6a371b43f15e2be40d6e67cd13d12
Fabian Oleksiuk <foleksiuk@sdncenter.pl>
2024-04-15 16:57:12 +0200

    Added user account page with basic data and operations.

---
+++ stores/useAccount.ts
@@ -0,0 +1,32 @@
+import type { Account } from '~/types'
+
+export const useAccount = defineStore('account', {
+  state: () => ({
+    data: null as Account | null,
+  }),
+  actions: {
+    async get() {
+      const runtimeConfig = useRuntimeConfig()
+      const { data } = await useFetch<Account>('account/me', {
+        baseURL: runtimeConfig.public.baseURL,
+        method: 'GET',
+        headers: {
+          Authorization: `Bearer ${useCookie('accessToken').value}`,
+        },
+      })
+
+      this.data = data.value
+    },
+    async changePassword(currentPassword: string, newPassword: string) {
+      const runtimeConfig = useRuntimeConfig()
+
+      await useFetch(`account/change-password?CurrentPassword=${currentPassword}&NewPassword=${newPassword}`, {
+        baseURL: runtimeConfig.public.baseURL,
+        method: 'POST',
+        headers: {
+          Authorization: `Bearer ${useCookie('accessToken').value}`,
+        },
+      })
+    },
+  },
+})
