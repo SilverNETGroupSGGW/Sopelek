f84e780da1ce6a8c419fe2cffb973c3bfb9b1314
Grzegorz02@ <grzegorz.perun@gmail.com>
2024-04-20 17:51:28 +0200

    feat: tab

--- components/base/tab.vue
+++ components/base/tab.vue
@@ -1,13 +1,29 @@
 <script setup lang="ts">
+import { TooltipArrow, TooltipContent, TooltipPortal, TooltipProvider, TooltipRoot, TooltipTrigger } from 'radix-vue'
+
 defineProps<{
   active?: boolean
+  label: string
   icon: any
   to: string
 }>()
 </script>
 
 <template>
-  <NuxtLink :to="to" class="-mx-3 flex size-12 items-center justify-center gap-3 rounded-full font-medium text-indigo-100 transition-colors duration-200 ease-in-out hover:bg-indigo-500" :class="[active ? 'bg-indigo-400' : 'bg-indigo-600']">
-    <component :is="icon" class="size-5 stroke-2" />
-  </NuxtLink>
+  <TooltipProvider :delay-duration="0">
+    <TooltipRoot>
+      <TooltipTrigger>
+        <NuxtLink :to="to" class="-mx-3 flex size-12 items-center justify-center gap-3 rounded-full font-medium text-indigo-100 transition-colors duration-200 ease-in-out hover:bg-indigo-500" :class="[active ? 'bg-indigo-400' : 'bg-indigo-600']">
+          <component :is="icon" class="size-5" stroke="2" />
+        </NuxtLink>
+      </TooltipTrigger>
+
+      <TooltipPortal>
+        <TooltipContent class="select-none rounded-lg border bg-white px-4 py-3 text-gray-700" :side-offset="24" side="right">
+          {{ label }}
+          <TooltipArrow class="fill-white" :width="8" />
+        </TooltipContent>
+      </TooltipPortal>
+    </TooltipRoot>
+  </TooltipProvider>
 </template>
