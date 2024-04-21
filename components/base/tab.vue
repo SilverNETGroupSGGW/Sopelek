96c1ffe4df94517f972b218531ed1bb82877940b
Fabian Oleksiuk <foleksiuk@sdncenter.pl>
2024-04-21 14:06:30 +0200

    Added logout button to sidebar.

--- components/base/tab.vue
+++ components/base/tab.vue
@@ -3,11 +3,19 @@ defineProps<{
   active?: boolean
   icon: any
   to: string
+  onClick?: () => Promise<void>
 }>()
 </script>
 
 <template>
-  <NuxtLink :to="to" class="-mx-3 flex size-12 items-center justify-center gap-3 rounded-full font-medium text-indigo-100 transition-colors duration-200 ease-in-out hover:bg-indigo-500" :class="[active ? 'bg-indigo-400' : 'bg-indigo-600']">
-    <component :is="icon" class="size-5 stroke-2" />
-  </NuxtLink>
+  <template v-if="onClick">
+    <button @click="onClick" class="-mx-3 cursor-pointer flex size-12 items-center justify-center gap-3 rounded-full font-medium text-indigo-100 transition-colors duration-200 ease-in-out hover:bg-indigo-500" :class="[active ? 'bg-indigo-400' : 'bg-indigo-600']">
+      <component :is="icon" class="size-5 stroke-2" />
+    </button>
+  </template>
+  <template v-else>
+    <NuxtLink :to="to" class="-mx-3 flex size-12 items-center justify-center gap-3 rounded-full font-medium text-indigo-100 transition-colors duration-200 ease-in-out hover:bg-indigo-500" :class="[active ? 'bg-indigo-400' : 'bg-indigo-600']">
+      <component :is="icon" class="size-5 stroke-2" />
+    </NuxtLink>
+  </template>
 </template>
