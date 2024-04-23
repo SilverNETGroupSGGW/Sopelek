8ab7d5a8deff4ec89ce40bfd82d7d664221d6bf1
Grzegorz02@ <grzegorz.perun@gmail.com>
2024-04-23 19:51:30 +0200

    refactor: cleanup tab logic

--- components/base/tab.vue
+++ components/base/tab.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import { NuxtLink } from '#components'
+
 defineProps<{
   active?: boolean
   icon: any
@@ -8,14 +10,7 @@ defineProps<{
 </script>
 
 <template>
-  <template v-if="onClick">
-    <button @click="onClick" class="-mx-3 cursor-pointer flex size-12 items-center justify-center gap-3 rounded-full font-medium text-indigo-100 transition-colors duration-200 ease-in-out hover:bg-indigo-500" :class="[active ? 'bg-indigo-400' : 'bg-indigo-600']">
-      <component :is="icon" class="size-5 stroke-2" />
-    </button>
-  </template>
-  <template v-else>
-    <NuxtLink :to="to" class="-mx-3 flex size-12 items-center justify-center gap-3 rounded-full font-medium text-indigo-100 transition-colors duration-200 ease-in-out hover:bg-indigo-500" :class="[active ? 'bg-indigo-400' : 'bg-indigo-600']">
-      <component :is="icon" class="size-5 stroke-2" />
-    </NuxtLink>
-  </template>
+  <component :is="onClick ? 'button' : NuxtLink" :to="onClick ? undefined : to" class="-mx-3 cursor-pointer flex size-12 items-center justify-center gap-3 rounded-full font-medium text-indigo-100 transition-colors duration-200 ease-in-out hover:bg-indigo-500" :class="[active ? 'bg-indigo-400' : 'bg-indigo-600']" @click="onClick">
+    <component :is="icon" class="size-5 stroke-2" />
+  </component>
 </template>
