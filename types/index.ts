bc7432cf126b58a2a933b91799eaad9b5a8f5926
Fabian Oleksiuk <foleksiuk@sdncenter.pl>
2024-04-24 00:46:10 +0200

    Added updated types/index.ts with Organization model.

--- types/index.ts
+++ types/index.ts
@@ -6,26 +6,25 @@ import { DayOfWeek, SubjectType } from './Subject'
 import type { Schedule } from './Schedule'
 import { DegreeOfStudy, StudyMode } from './Schedule'
 import type { Group } from './Group'
-import type { Lecturer } from './Lecturer'; import type { Lesson } from './Lesson'
+import type { Lecturer } from './Lecturer'
+import type { Lesson } from './Lesson'
 import type { StudyProgram } from './StudyProgram'
 import type { Account } from './Account'
-import type { AccountTokens } from './AccountTokens'
 import type { Organization } from './Organization'
 
 export type {
-    Account,
-    AccountTokens,
-    Subject,
-    Lecturer,
-    Classroom,
-    Schedule,
-    Lesson,
-    Group,
-    SubjectConflict,
-    SubjectConflictItem,
-    BaseResponse,
-    StudyProgram,
-    Organization
+  Account,
+  Subject,
+  Lecturer,
+  Classroom,
+  Schedule,
+  Lesson,
+  Group,
+  SubjectConflict,
+  SubjectConflictItem,
+  BaseResponse,
+  StudyProgram,
+  Organization,
 }
 
 export { AcademicDegree, DayOfWeek, SubjectType, StudyMode, DegreeOfStudy }
