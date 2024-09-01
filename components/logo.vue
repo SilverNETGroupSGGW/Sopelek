b9d839bde7db692d733fd7a908f264cc691f4069
Fabian Oleksiuk <Fabian.Oleksiuk@fenergo.com>
2024-09-01 13:32:47 +0200

    Changed redirect after sidebar logo click. Added logo color prop.

--- components/logo.vue
+++ components/logo.vue
@@ -1,15 +1,27 @@
 <script setup lang="ts">
+defineProps<{
+  color: 'indigo' | 'white' | 'gray'
+  linkUrl?: string
+}>()
+
+function colorClass(color: 'indigo' | 'white' | 'gray') {
+  return {
+    'bg-indigo-50': color === 'indigo',
+    'bg-white': color === 'white',
+    'bg-gray-400': color === 'gray',
+  }
+}
 </script>
 
 <template>
-  <NuxtLink to="/" class="block space-y-0.5">
-    <div class="h-1.5 w-6 rounded-3xl" :class="$attrs.class" />
+  <NuxtLink :to="linkUrl ? linkUrl : '/'" class="block space-y-0.5">
+    <div class="h-1.5 w-6 rounded-3xl" :class="colorClass(color)" />
     <div class="ml-2">
-      <div class="h-1.5 w-6 rounded-3xl" :class="$attrs.class" />
+      <div class="h-1.5 w-6 rounded-3xl" :class="colorClass(color)" />
     </div>
     <div class="ml-1.5">
-      <div class="h-1.5 w-6 rounded-3xl" :class="$attrs.class" />
+      <div class="h-1.5 w-6 rounded-3xl" :class="colorClass(color)" />
     </div>
-    <div class="h-1.5 w-6 rounded-3xl" :class="$attrs.class" />
+    <div class="h-1.5 w-6 rounded-3xl" :class="colorClass(color)" />
   </NuxtLink>
 </template>
