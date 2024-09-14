524ad54c09ab64a7c7fd10b057f17f20b19f30b6
Fabian Oleksiuk <Fabian.Oleksiuk@fenergo.com>
2024-09-14 20:50:22 +0200

    Added env variables preview page.

---
+++ pages/env.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { useRuntimeConfig } from '#imports'
+
+const config = useRuntimeConfig()
+
+const apiUrl = config.public.baseURL
+</script>
+
+<template>
+  <div>
+    <h1>Environment Variables</h1>
+    <ul>
+      <li>API_URL: {{ apiUrl }}</li>
+    </ul>
+  </div>
+</template>
