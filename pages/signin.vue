f3ad42ff566f8cbe2600fceb7a567017f426f6f0
Fabian Oleksiuk <Fabian.Oleksiuk@fenergo.com>
2024-10-07 19:45:06 +0200

    Added signin page refactor to use api request from store.

--- pages/signin.vue
+++ pages/signin.vue
@@ -1,49 +1,30 @@
 <script setup lang="ts">
 import { IconAt, IconKey } from '@tabler/icons-vue'
-import type { UserTokens } from '~/types'
-
-interface User {
-  email: string
-  password: string
-  deviceToken: string
-}
-
-const runtimeConfig = useRuntimeConfig()
+import { useAuthApi } from '~/stores/api/useAuthApi'
 
 definePageMeta({
   layout: 'user',
 })
 
-const form = reactive<User>({
-  email: '',
-  password: '',
-  deviceToken: '',
-})
+const authApi = useAuthApi()
 
-const isSubmitting = ref(false)
+const email = ref<string>('')
+const password = ref<string>('')
+const isSubmitting = ref<boolean>(false)
 
 async function handleFormSubmit() {
   isSubmitting.value = true
-  try {
-    const { refreshToken, accessToken } = await $fetch<UserTokens>('tokens/login', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(form),
-      baseURL: runtimeConfig.public.baseURL,
-    })
-    isSubmitting.value = false
+  const response = await authApi.getTokensAsync(email.value, password.value)
+  isSubmitting.value = false
 
-    useCookie('accessToken').value = accessToken
-    useCookie('refreshToken').value = refreshToken
+  if (response.hasError) {
+    return Promise.reject(response.errorMessage)
+  }
 
-    await navigateTo('/portal/')
-  }
-  catch (error) {
-    isSubmitting.value = false
-    return Promise.reject(error)
-  }
+  useCookie('accessToken').value = response.data?.accessToken
+  useCookie('refreshToken').value = response.data?.refreshToken
+
+  await navigateTo('/portal/')
 }
 </script>
 
@@ -58,15 +39,39 @@ async function handleFormSubmit() {
 
     <form class="w-full" @submit.prevent="handleFormSubmit">
       <div class="mb-8 space-y-5">
-        <base-input v-model="form.email" :icon="IconAt" type="email" placeholder="pXXXXXX@sggw.edu.pl" label="Adres e-mail" autocomplete="false" />
-        <base-input v-model="form.password" :icon="IconKey" type="password" placeholder="********" label="Hasło" autocomplete="false" />
+        <base-input
+          v-model="email"
+          :icon="IconAt"
+          type="email"
+          placeholder="pXXXXXX@sggw.edu.pl"
+          label="Adres e-mail"
+          autocomplete="false"
+        />
+        <base-input
+          v-model="password"
+          :icon="IconKey"
+          type="password"
+          placeholder="********"
+          label="Hasło"
+          autocomplete="false"
+        />
       </div>
 
       <div class="flex w-full justify-end">
-        <base-button class="mr-2" variant="secondary" :disabled="isSubmitting" :loading="isSubmitting" to="/">
+        <base-button
+          class="mr-2"
+          variant="secondary"
+          :disabled="isSubmitting"
+          :loading="isSubmitting" to="/"
+        >
           Powrót
         </base-button>
-        <base-button variant="primary" type="submit" :disabled="isSubmitting" :loading="isSubmitting">
+        <base-button
+          variant="primary"
+          type="submit"
+          :disabled="isSubmitting"
+          :loading="isSubmitting"
+        >
           Zaloguj się
         </base-button>
       </div>
